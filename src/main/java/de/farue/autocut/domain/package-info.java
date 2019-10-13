a0b97f3e1c15c3420031de2630ff824bc468d5ea
Stefan Brunecker <stefan.brunecker@rwth-aachen.de>
2019-10-13 19:17:58 +0200

    Generated with JHipster 6.0.1

---
+++ src/main/java/de/farue/autocut/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * JPA domain objects.
+ */
+package de.farue.autocut.domain;
---
+++ src/main/java/de/farue/autocut/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package de.farue.autocut.repository;
+
+import de.farue.autocut.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
