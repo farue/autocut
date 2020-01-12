4d41fb61a72f8dfb4b3c85c6995fe8c6dd4e8cf5
Stefan Brunecker <stefan.brunecker@rwth-aachen.de>
2020-01-12 20:24:03 +0100

    Fix import

--- src/test/java/de/farue/autocut/service/MailServiceIT.java
+++ src/test/java/de/farue/autocut/service/MailServiceIT.java
@@ -32,7 +32,7 @@ import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
