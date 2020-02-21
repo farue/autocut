3db54dc0cdbafd9d2846e90a22463d85bac65da2
Stefan Brunecker <stefan.brunecker@rwth-aachen.de>
2020-02-22 00:59:41 +0100

    Remove team suffix from path

--- src/main/webapp/app/teams/tools-team/tools-team.route.ts
+++ src/main/webapp/app/teams/tools-team/tools-team.route.ts
@@ -1,10 +1,10 @@
-import {Route} from '@angular/router';
+import { Route } from '@angular/router';
 
-import {UserRouteAccessService} from 'app/core/auth/user-route-access-service';
-import {ToolsTeamComponent} from './tools-team.component';
+import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
+import { ToolsTeamComponent } from './tools-team.component';
 
 export const TOOLS_TEAM_ROUTE: Route = {
-  path: 'tools-team',
+  path: 'tools',
   component: ToolsTeamComponent,
   data: {
     authorities: [],
