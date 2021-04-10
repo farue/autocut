c845f58a73c792bc15ad2fb0e371f7e6c9676f44
Stefan Brunecker <stefan.brunecker@rwth-aachen.de>
2021-04-10 18:31:28 +0200

    Replace carousel on home page

--- src/main/webapp/app/shared/material.module.ts
+++ src/main/webapp/app/shared/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCarouselModule } from '@ngbmodule/material-carousel';
 
-@NgModule({ exports: [FlexLayoutModule, MatCardModule] })
+@NgModule({ exports: [FlexLayoutModule, MatCardModule, MatCarouselModule] })
 export class MaterialModule {}
