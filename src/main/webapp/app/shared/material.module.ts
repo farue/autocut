e7dfb1a6c8766c9d664571117dca2436d607f4ac
Stefan Brunecker <stefan.brunecker@rwth-aachen.de>
2021-04-11 21:31:05 +0200

    Redesign login page with material design

--- src/main/webapp/app/shared/material.module.ts
+++ src/main/webapp/app/shared/material.module.ts
@@ -2,6 +2,30 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCarouselModule } from '@ngbmodule/material-carousel';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from '@angular/material/core';
+import { MatPasswordStrengthModule } from '@angular-material-extensions/password-strength';
 
-@NgModule({ exports: [FlexLayoutModule, MatCardModule, MatCarouselModule] })
+@NgModule({
+  exports: [
+    FlexLayoutModule,
+    MatCardModule,
+    MatCarouselModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCheckboxModule,
+    MatDividerModule,
+    MatIconModule,
+    MatRippleModule,
+    MatPasswordStrengthModule,
+  ],
+})
 export class MaterialModule {}
