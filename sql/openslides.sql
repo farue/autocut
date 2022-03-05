e8db41f57a6d2d334e8ecc08198ce4f60d34c619
Stefan Brunecker <stefan.brunecker@rwth-aachen.de>
2022-03-06 00:54:55 +0100

    Update openslides sql

--- sql/openslides.sql
+++ sql/openslides.sql
@@ -2,10 +2,17 @@
 # they will not be included.
 
 # generate user import data
-set @vv_date = '2021-08-31';
+set @vv_date = '2022-03-19';
 select null                                                                as 'Title',
        t.first_name                                                        as 'Given name',
        t.last_name                                                         as 'Surname',
+       '1'                                                                 as 'Active',
+       '1'                                                                 as 'Natural person',
+       null                                                                as 'Initial password',
+       u.email                                                             as 'Email',
+       u.login                                                             as 'Username',
+       null                                                                as 'Gender',
+       u.id                                                                as 'Participant number',
        (select group_concat(teamtable.teamname SEPARATOR ', ')
         from (select case
                          when team.name = 'SPOKESPERSON' then 'Wohnheimsprecher/in'
@@ -24,7 +31,9 @@ select null                                                                as 'T
               where tm.tenant_id = t.id
                 and tm.start <= @vv_date
                 and (tm.end is null or tm.end > @vv_date)) as teamtable)   as 'Structure level',
-       u.id                                                                as 'Participant number',
+       '1'                                                                 as 'Vote weight',
+       null                                                                as 'Comment',
+       null                                                                as 'Is present',
        (select if(spezialRoles is null, 'Delegates', concat('Delegates', ', ', spezialRoles))
         from (select group_concat(DISTINCT roles.role SEPARATOR ', ') as spezialRoles
               from (select case
@@ -35,16 +44,7 @@ select null                                                                as 'T
                              inner join team on tm.team_id = team.id
                     where tm.tenant_id = t.id
                       and tm.start <= @vv_date
-                      and (tm.end is null or tm.end > @vv_date)) roles) r) as 'Groups',
-       null                                                                as 'Comment',
-       true                                                                as 'Is active',
-       null                                                                as 'Is present',
-       null                                                                as 'Is a committee',
-       null                                                                as 'Initial password',
-       u.email                                                             as 'Email',
-       u.login                                                             as 'Username',
-       null                                                                as 'Gender',
-       null                                                                as 'Vote weight'
+                      and (tm.end is null or tm.end > @vv_date)) roles) r) as 'Groups'
 from tenant t
          inner join jhi_user u on t.user_id = u.id
          inner join lease l on t.lease_id = l.id
