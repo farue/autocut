68c1510fb4c7a9ad082d6bd59bff700faacec02e
Stefan Brunecker <stefan.brunecker@rwth-aachen.de>
2022-03-13 16:56:17 +0100

    Remove services from proxy so page reload works
    With /services the calls to our services page were proxied to the
    server so page reloads at e.g. /services/transactions didn't work.

--- webpack/proxy.conf.js
+++ webpack/proxy.conf.js
@@ -1,7 +1,8 @@
 function setupProxy({ tls }) {
   const conf = [
     {
-      context: ['/api', '/services', '/management', '/v3/api-docs', '/h2-console', '/auth', '/health'],
+      // AC: Removed /services
+      context: ['/api', '/management', '/v3/api-docs', '/h2-console', '/auth', '/health'],
       target: `http${tls ? 's' : ''}://localhost:8080`,
       secure: false,
       changeOrigin: tls,
